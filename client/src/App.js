74cdb1f22d52ff0b7b43508686728531a12e2295
Eslam09 <95702158+Eslam09@users.noreply.github.com>
2022-04-08 15:44:48 +0200

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-
+import Login from './compunint/Login';
+import Home from './compunint/Home';
+import './style/style.css';
 function App() {
-  const [data, setData] = React.useState(null);
+  const userProto = {
+    userName: '',
+    password: '',
+  };
+  const [data, setData] = React.useState({});
+  const [success, setSuccess] = React.useState(false);
+  const [user, setUser] = React.useState(userProto);
 
+  // fetch data from api
   React.useEffect(() => {
     fetch('/api')
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => setData(data));
   }, []);
+
+  //Take input from Form
+  function changehandler(event) {
+    return setUser((olduser) => {
+      return {
+        ...olduser,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+  function submithandler(event) {
+    event.preventDefault();
+    if (data.userName === user.userName && data.password === user.password) {
+      setSuccess(true);
+    } else {
+      setSuccess(false);
+      setUser(userProto);
+    }
+  }
+  console.log(success);
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>{!data ? 'Loading...' : data}</p>
-      </header>
+      {!success ? (
+        <Login
+          user={user}
+          changehandler={changehandler}
+          submithandler={submithandler}
+        />
+      ) : (
+        <Home />
+      )}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
+var fs = require('fs');
 const path = require('path');
+var https = require('https');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-// Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// Handle GET requests to /api route
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from Heroku!' });
+  res.json({ userName: 'Hamdy', password: '01273050924' });
 });
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
