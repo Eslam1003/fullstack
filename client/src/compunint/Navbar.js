323d587f9fd2f3cc81ca39f242f6ddb1516bedde
Eslam09 <95702158+Eslam09@users.noreply.github.com>
2022-04-18 16:32:49 +0200

    s

---
+++ client/src/compunint/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navbar = (props) => {
+  return (
+    <div>
+      <ul className='nav--ul'>
+        <li
+          className='nave--home active'
+          id='home'
+          onClick={props.clickhandler}
+        >
+          Home
+        </li>
+        <li className='nave--bills' id='bills' onClick={props.clickhandler}>
+          Bills
+        </li>
+        <li className='nave--add' id='add' onClick={props.clickhandler}>
+          Add
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
